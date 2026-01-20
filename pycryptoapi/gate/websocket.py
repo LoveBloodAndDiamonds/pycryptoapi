536b2467cd383efc1c367672b0bd88f709859bb4
lovebloodanddiamonds <blowyourbra1n@mail.ru>
2026-01-20 19:39:21 +0300

    Fix gateio bug

--- pycryptoapi/gate/websocket.py
+++ pycryptoapi/gate/websocket.py
@@ -1,16 +1,21 @@
-__all__ = ["GateWebsocket", "GateSocketManager", ]
+__all__ = [
+    "GateWebsocket",
+    "GateSocketManager",
+]
 
 import json
-from typing import Optional, Union, List, Literal, Callable, Awaitable, Dict, Tuple
 import time
+from typing import Awaitable, Callable, List, Literal, Optional, Tuple, Union
 
-from ..abstract import AbstractWebsocket, AbstractSocketManager
-from ..enums import MarketType, Timeframe, Exchange
-from ..exceptions import MarketException, TimeframeException
+import orjson
+from websockets.asyncio.client import ClientConnection
+
+from ..abstract import AbstractSocketManager, AbstractWebsocket
+from ..enums import MarketType, Timeframe
+from ..exceptions import MarketException
 
 
 class GateWebsocket(AbstractWebsocket):
-
     @property
     def _connection_uri(self) -> str:
         if self._market_type == MarketType.SPOT:
@@ -22,7 +27,6 @@ class GateWebsocket(AbstractWebsocket):
 
     @property
     def _subscribe_message(self) -> Union[str, List[str]]:
-
         if self._market_type == MarketType.SPOT:
             if self._topic == "spot.trades":
                 payload = []
@@ -70,16 +74,54 @@ class GateWebsocket(AbstractWebsocket):
         else:
             raise MarketException()
 
+    @property
+    def _pong_message(self) -> str:
+        if self._market_type == MarketType.SPOT:
+            return json.dumps({"time": int(time.time()), "channel": "spot.pong"})
+        elif self._market_type == MarketType.FUTURES:
+            return json.dumps({"time": int(time.time()), "channel": "futures.pong"})
+        else:
+            raise MarketException()
+
+    async def _handler(self, conn: ClientConnection) -> None:
+        """
+        Принимает управление над активным подключением.
+
+        Параметры:
+            conn (ClientConnection): Активное WebSocket соединение.
+        """
+        # Слушаем входящие сообщения
+        while self._is_active:
+            try:
+                message = await conn.recv()
+                if message.lower() == "ping":
+                    await conn.send(self._pong_message)
+                    self._logger.debug(f"Sent pong message: {self._pong_message}")
+                self._last_message_time = time.time()
+                self._logger.trace(f"{self} Received message: {message}")
+                await self._queue.put(orjson.loads(message))
+            except orjson.JSONDecodeError:
+                if message not in ["ping", "pong"]:
+                    self._logger.error(
+                        f"{self} orjson.JSONDecodeError whilte handling message: {message}"
+                    )
+                else:
+                    self._logger.debug(f"{self} Received ping message: {message}")
+            except Exception as e:
+                self._logger.error(
+                    f"{self} Error({type(e)}) while handling message: {e}"
+                )
+                break
+
 
 class GateSocketManager(AbstractSocketManager):
-
     @classmethod
     def aggtrades_socket(
-            cls,
-            market_type: MarketType,
-            tickers: List[str] | Tuple[str, ...],
-            callback: Callable[..., Awaitable],
-            **kwargs
+        cls,
+        market_type: MarketType,
+        tickers: List[str] | Tuple[str, ...],
+        callback: Callable[..., Awaitable],
+        **kwargs,
     ) -> GateWebsocket:
         if market_type == MarketType.SPOT:
             topic: str = "spot.trades"
@@ -92,27 +134,27 @@ class GateSocketManager(AbstractSocketManager):
             tickers=tickers,
             market_type=market_type,
             callback=callback,
-            **kwargs
+            **kwargs,
         )
 
     @classmethod
     def klines_socket(
-            cls,
-            tickers: List[str] | Tuple[str, ...],
-            timeframe: Timeframe,
-            market_type: MarketType,
-            callback: Callable[..., Awaitable],
-            **kwargs
+        cls,
+        tickers: List[str] | Tuple[str, ...],
+        timeframe: Timeframe,
+        market_type: MarketType,
+        callback: Callable[..., Awaitable],
+        **kwargs,
     ) -> GateWebsocket:
         raise NotImplementedError()
 
     @classmethod
     def tickers_socket(
-            cls,
-            market_type: MarketType,
-            callback: Callable[..., Awaitable],
-            timezone: Literal[""] = "+8",
-            **kwargs
+        cls,
+        market_type: MarketType,
+        callback: Callable[..., Awaitable],
+        timezone: Literal[""] = "+8",
+        **kwargs,
     ) -> GateWebsocket:
         raise NotImplementedError()
 
